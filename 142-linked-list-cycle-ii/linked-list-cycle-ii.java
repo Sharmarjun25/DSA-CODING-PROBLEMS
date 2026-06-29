b45324f25851ed654a6c67bf694d5c7483ab1075
Arjun Sharma <arjun.2428it378@kiet.edu>
2026-06-29 22:26:18 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (49.32%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -11,7 +11,7 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        HashMap<ListNode , Integer> map = new HashMap<>();
+        /*HashMap<ListNode , Integer> map = new HashMap<>();
         ListNode temp  = head;
         while(temp != null){
             if(map.containsKey(temp)){
@@ -19,6 +19,24 @@ public class Solution {
             }
             map.put(temp , 1);
             temp = temp.next;
+        }
+        return null;*/
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(slow == fast){
+                slow = head;
+                // = fast.next;
+                //slow = slow.next;
+                while(slow != fast){
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
+               
+            }
         }
         return null;
         
