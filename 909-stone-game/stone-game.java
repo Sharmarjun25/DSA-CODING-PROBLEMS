72f5e94e7a8028b47c4284a220d0145faf6976b3
Arjun Sharma <arjun.2428it378@kiet.edu>
2026-08-16 12:53:45 +0530

    Time: 5 ms (31.90%) | Memory: 43.1 MB (66.80%) - LeetSync

---
+++ 909-stone-game/stone-game.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        Arrays.sort(piles);
+        int n = piles.length;
+        int alice = 0;
+        int bob = 0;
+        for(int i = n-1 ; i >= 0 ; i--){
+            if(i % 2 != 0){
+                alice += piles[i];
+            }else{
+                bob += piles[i];
+            }
+        }
+        if(alice > bob){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
